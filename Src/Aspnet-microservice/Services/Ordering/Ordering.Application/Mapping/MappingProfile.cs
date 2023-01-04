bb670d76626ea0edb6b943a4b9e5d63ee8d0c731
sushantlearnazure <121449591+sushantlearnazure@users.noreply.github.com>
2023-01-04 17:10:54 +0530

    Added ordering Application layer in project

---
+++ Src/Aspnet-microservice/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Command.CheckoutOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrderList;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+        }
+    }
+}
