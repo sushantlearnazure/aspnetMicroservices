dbd17c46a302ebcb8507a23dae8c74d7827d2eab
sushantlearnazure <121449591+sushantlearnazure@users.noreply.github.com>
2023-01-07 11:35:14 +0530

    made changes to ordering API

---
+++ Src/Aspnet-microservice/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository: RepositoryBase<Order>,IOrderRepository
+    {
+        public OrderRepository(OrderContext context):base(context) { }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderlist = await _dbContext.Orders.Where(o => o.UserName == userName).ToListAsync();
+            return orderlist;
+        }
+    }
+}
